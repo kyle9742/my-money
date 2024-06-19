5afb829abda6758fa3fde2d03e77260cb930b922
it <kyle9742@naver.com>
2024-06-19 14:53:33 +0900

    Home 버튼 업데이트

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styles from './Navbar.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li className={styles.title}>myMoney</li>
+        <li className={styles.title}>
+          <Link to="/">myMoney</Link>
+        </li>
 
-        <li><Link to="/login">로그인</Link></li>
-        <li><Link to="/signup">가입</Link></li>
+        <li>
+          <Link to="/login">로그인</Link>
+        </li>
+        <li>
+          <Link to="/signup">가입</Link>
+        </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
