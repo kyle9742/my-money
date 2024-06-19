e19a62b7578d24427a4fe4f18d6d2931eb6b17c5
it <kyle9742@naver.com>
2024-06-19 14:21:47 +0900

    네브바 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route>
+      <Navbar />
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-        </Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>myMoney</li>
+
+        <li><Link to="/login">로그인</Link></li>
+        <li><Link to="/signup">가입</Link></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
