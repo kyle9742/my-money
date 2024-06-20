fba8008cee7dcfaa9b27097a357a7586524ab43a
it <kyle9742@naver.com>
2024-06-20 11:49:59 +0900

    거래내역 폼 추가

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from './Home.module.css';
+import TransactionForm from '../../components/TransactionForm';
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+    <div className={styles.content}>
+      거래 내역
     </div>
+    <div className={styles.sidebar}>
+      <TransactionForm />
+    </div>
+  </div>
   )
 }
 
